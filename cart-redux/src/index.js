433e01f49f969f976532f0980076c1b8ca466e54
kemmie <kemmie.xd@gmail.com>
2019-04-08 17:14:38 +0700

    Start Cart Redux Project

---
+++ cart-redux/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import appReducers from './reducers/index';
+
+const store = createStore(
+  appReducers
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
